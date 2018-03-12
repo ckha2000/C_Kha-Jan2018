f7148c08b0b901c442dd7f4e75421d59f8773378
ckha2000 <christopher.kha2000@gmail.com>
2018-03-12 09:50:38 -0700

    update checkpoint 1

--- TextExcel/src/textExcel/TextCell.java
+++ TextExcel/src/textExcel/TextCell.java
@@ -1,22 +1,32 @@
 package textExcel;
 
-public class EmptyCell implements Cell {
+public class TextCell implements Cell {
+	String text;
 	
-	public EmptyCell() {
+	public TextCell(String input) {
+		text = input;
 	}
 	
 	@Override
 	// text for spreadsheet cell display, must be exactly length 10
 	public String abbreviatedCellText() {
 		// TODO Auto-generated method stub
-		return "          ";
+		if(text.length() > 10) {
+			return text.substring(0, 9);
+		} else {
+			String returnString = text;
+			for(int i = 0; i < (10 - text.length()); i++ ) {
+				returnString += " ";
+			}
+			return returnString;
+		}
 	}
 
 	@Override
 	// text for individual cell inspection, not truncated or padded
 	public String fullCellText() {
 		// TODO Auto-generated method stub
-		return "";
+		return text;
 	}
 
 }
