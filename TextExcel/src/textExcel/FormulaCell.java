d52c790c97e46d064bff9fe5202c04ac467eff10
ckha2000 <christopher.kha2000@gmail.com>
2018-03-28 11:30:52 -0700

    checkpoint 5

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -1,9 +1,38 @@
 package textExcel;
 
 public class FormulaCell extends RealCell {
+	private Spreadsheet grid;
 	
 	public FormulaCell(String inputValue) {
 		super(inputValue);
+		grid = TextExcel.userSheet;
+	}
+	
+	public double computeFormula(String range, boolean isAverage) {
+		
+		String[] splitRange = range.split("-");
+		int r1 = Integer.parseInt(splitRange[0].substring(1));
+		int r2 = Integer.parseInt(splitRange[1].substring(1));
+		
+		char c1 = splitRange[0].charAt(0);
+		char c2 = splitRange[1].charAt(0);
+		
+		double sum = 0.0;
+		int numValues = 0;
+		
+		for(int r = r1; r <= r2; r++) {
+			for(char c = c1; c <= c2; c++) {
+				Location loc = new SpreadsheetLocation("" + c + r);
+				sum += ((RealCell) (grid.getCell(loc))).getDoubleValue();
+				numValues++;
+			}
+		}
+		
+		if(isAverage) {
+			return sum/numValues;
+		}else {
+			return sum;
+		}
 	}
 	
 	public double getDoubleValue() {
@@ -13,40 +42,41 @@ public class FormulaCell extends RealCell {
 		double answer;
 		Location loc;
 		
-		if(Character.isDigit(splitInput[0].indexOf(0))) {
-			answer = Double.parseDouble(splitInput[0]);
-		}else if(Character.isDigit(splitInput[0].indexOf(1))) { 	//the value is a cell or a formula
-			loc = new SpreadsheetLocation(splitInput[0]);
-			answer = ((RealCell)TextExcel.userSheet.getCell(loc)).getDoubleValue();
-		}else {
-			answer = 0.0;
+		if(splitInput[0].equalsIgnoreCase("sum")) {
+			return computeFormula(splitInput[1], false);
+		}else if(splitInput[0].equalsIgnoreCase("avg")) {
+			return computeFormula(splitInput[1], true);
 		}
 		
-		System.out.println(answer);
+ 		if(Character.isDigit(splitInput[0].charAt(0)) || splitInput[0].charAt(0) == '-') { // its a number
+			answer = Double.parseDouble(splitInput[0]);
+		}else {
+			loc = new SpreadsheetLocation(splitInput[0]);
+			answer = ((RealCell) (grid.getCell(loc))).getDoubleValue();
+		}
 		
 		for(int i = 1; i < splitInput.length; i += 2) {
 			double nextOperand;
 			
-			if(Character.isDigit(splitInput[i+1].indexOf(0))) {
+			if(Character.isDigit(splitInput[i+1].charAt(0)) || splitInput[i+1].charAt(0) == '-') {
 				nextOperand = Double.parseDouble(splitInput[i+1]);
 			}else {
 				loc = new SpreadsheetLocation(splitInput[i+1]);
-				nextOperand = ((RealCell)TextExcel.userSheet.getCell(loc)).getDoubleValue();
+				Cell tempCell = grid.getCell(loc);
+				nextOperand = ((RealCell) tempCell).getDoubleValue();
+			
+			
+				if(splitInput[i].equals("+")){
+					answer += nextOperand;
+				}else if(splitInput[i].equals("-")) {
+		         	answer -= nextOperand;
+		        }else if(splitInput[i].equals("*")){
+		        	answer *= nextOperand;
+		        }else if(splitInput[i].equals("/")) {
+		        	answer /= nextOperand;
+		        }
 			}
-			
-			
-			if(splitInput[i].equals("+")){
-				answer += nextOperand;
-			}else if(splitInput[i].equals("-")) {
-	         	answer -= nextOperand;
-	        }else if(splitInput[i].equals("*")){
-	        	answer *= nextOperand;
-	        }else if(splitInput[i].equals("/")) {
-	        	answer /= nextOperand;
-	        }
 		}
-		
 		return answer;
 	}
-	
 }
